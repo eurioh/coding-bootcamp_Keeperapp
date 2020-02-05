5ec1353d8c31d357dae56bc9f318d8e730d2090b
eurioh <euri.elliot.oh@gmail.com>
2020-02-04 22:14:13 -0500

    add arrow function and refactor the source code

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,23 @@ import Footer from "./components/Footer";
 import Note from "./components/Note";
 import notes from "./notes.js";
 
-function createNote(note){
-return (
-    // will send Note.jsx component
-    <Note 
-        key={note.key}
-        //important to use it when using .map(), key will organize the items inside when mapping
-        title={note.title}
-        // note will contain title property
-        content={note.content}
-        // need curlybarces because of js code
-    />
-)}; 
-//commented out to create arrow (=>) function
-//can be defined inside the createNotes function of {notes.map(createNotes)} 
 
 function App(){
     return(
         <div>
             <Header />
-                {notes.map(createNote)}
-                    
-                {/* pull directly from notes.js file -> notes.map */}
+                {notes.map(note => (
+                    <Note
+                        key={note.key}
+                        title={note.title}
+                        content={note.content}
+                    />
+                ))}
             <Footer />
         </div>
     );
 }
+
+
 
 export default App;
